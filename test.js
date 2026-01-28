b4f24d1040aeaf0d7cb3471e24ca08d42dfca728
xjwsx <wlstmdghs@naver.com>
2026-01-28 21:28:39 +0900

    feat: JavaScript 입문 수업 자료 추가
    - Hello World 및 출력 방법 (01-hello-world.html)
    - 변수와 데이터 타입 (02-variables.html)
    - 연산자 (03-operators.html)
    - DOM 조작 기초 (04-dom-basics.html)
    - 이벤트 처리 (05-events.html)
    - README 및 기타 실습 파일

---
+++ test.js
@@ -0,0 +1,13 @@
+const name = "정지희";
+const age = 32;
+const hobby = "코딩";
+const plus = age + 2;
+
+const person = {
+  name: name,
+  age: age,
+  hobby: hobby,
+};
+console.log(name, age, hobby);
+console.log(plus);
+console.log(person.name);
